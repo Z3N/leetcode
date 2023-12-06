e97bcfcfb66555ab328514cf84b4dede91b9c75a
Z3N <@github.com/Z3N>
2023-12-06 09:31:56 +0300

    Problem 909 solved, BFS memoized.
    Took 1 hour 49 minutes

--- s210_course_schedule_ii/src/lib.rs
+++ s210_course_schedule_ii/src/lib.rs
@@ -34,17 +34,3 @@ impl Solution {
     }
 }
 struct Solution;
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
